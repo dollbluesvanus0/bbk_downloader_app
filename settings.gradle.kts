4dcc88e2a670f1fd24d6fd804c282d77affdeaef
dollbluesvanus0 <vgordymov16@gmail.com>
2026-07-04 15:44:03 +0800

    Rename app to OOD (Oneplus ota downloader)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -29,5 +29,5 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
 
-rootProject.name = "BBK Downloader"
+rootProject.name = "OOD (Oneplus ota downloader)"
 include(":app")
